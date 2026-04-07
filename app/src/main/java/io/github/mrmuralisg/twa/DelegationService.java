e09d653de517983f7286a78350a76ee78a2e3f77
Murali <mr.muralisg@gmail.com>
2026-04-07 09:45:02 +0800

    seond commit

---
+++ app/src/main/java/io/github/mrmuralisg/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.mrmuralisg.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
